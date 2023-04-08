4c54d8e6cdaf3cfe8ff46511752ef0990144a6bb
Dmitry Fedorov <dmitry_fedorov1@epam.com>
2023-04-08 12:52:46 +0300

    refactor: delete unused code

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-
-// import { AppModule } from './app.module';
-
-// const port = process.env.PORT || 4000;
-
-// async function bootstrap() {
-
-//   let app: any;
-
-//   try {
-//     app = await NestFactory.create(AppModule);
-//   } catch (error) {
-//     console.log({ error });
-//   }
-
-//   app.enableCors({
-//     origin: (req, callback) => callback(null, true),
-//   });
-
-//   await app.listen(port);
-// }
-// bootstrap()
-//   .then(() => {
-//     console.log('App is running on %s port', port);
-//   })
-//   .catch(err => {
-//     console.log({ err });
-//   });
-
 import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
